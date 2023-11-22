591c7d48a324f13aab7be63f89ab1e67fd786865
zuhdy-ev <ardhiekov@gmail.com>
2023-11-22 20:50:05 +0700

    variables and data types

--- firstProgram.dart
+++ firstProgram.dart
@@ -1,3 +1,16 @@
 void main() {
-  print('Hello World');
+  var name = 'Voyager I';
+  var year = 1977;
+  var antennaDiameter = 3.7;
+  var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+  var image = {
+    'tags': ['saturn'],
+    'url': '//path/to/saturn.jpg'
+  };
+
+  print('name: $name');
+  print('year: $year');
+  print('antenna diameter: $antennaDiameter');
+  print('$flybyObjects -- like an array in JS');
+  print('$image -- like an object in JS');
 }
