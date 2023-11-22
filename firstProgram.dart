ac051c89015d08ff66956f58956996e96f7c4d5d
zuhdy-ev <ardhiekov@gmail.com>
2023-11-22 20:58:49 +0700

    data type boolean

--- firstProgram.dart
+++ firstProgram.dart
@@ -1,16 +1,24 @@
 void main() {
-  var name = 'Voyager I';
-  var year = 1977;
-  var antennaDiameter = 3.7;
-  var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+  var name = 'Voyager I'; //string
+  var year = 1977; //int
+  var antennaDiameter = 3.7; //double (like float in JS)
+  var flybyObjects = [
+    'Jupiter',
+    'Saturn',
+    'Uranus',
+    'Neptune'
+  ]; //list (like array in JS)
   var image = {
     'tags': ['saturn'],
     'url': '//path/to/saturn.jpg'
-  };
+  }; //map (like object in JS)
+  var isLoggedIn = false; //boolean
 
   print('name: $name');
   print('year: $year');
   print('antenna diameter: $antennaDiameter');
-  print('$flybyObjects -- like an array in JS');
-  print('$image -- like an object in JS');
+  print('the biggest planet is ${flybyObjects[0]}');
+  print(image);
+  // ignore: dead_code
+  print(isLoggedIn ? 'Logged in' : 'Logged out');
 }
